0d4f3a98ab5161520e729e0364e5afec6b32f63d
universe-walker <nikitamotlich@gmail.com>
2022-07-25 15:27:55 +0300

    init

---
+++ src/test/java/com/nm/Militaryoffice/MilitaryOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nm.Militaryoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitaryOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
