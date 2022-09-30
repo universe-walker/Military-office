96c2095db34beb938b44d1b197b2d9c209d18859
universe-walker <nikitamotlich@gmail.com>
2022-09-30 21:09:15 +0300

    Сделал класс дата классом, добавил пустой конструктор

--- src/main/java/com/nm/Militaryoffice/model/Education.kt
+++ src/main/java/com/nm/Militaryoffice/model/Education.kt
@@ -5,10 +5,19 @@ import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
-class Education(@NotEmpty var id_education: Long,
+data class Education(@NotEmpty var id_education: Long,
                 @NotEmpty var id: Long,
                 @NotEmpty @Size(max=30) var education_level: String,
                 @NotEmpty var location: String,
                 @NotNull var edu_start_date: Date,
                 var edu_end_date: Date?
-                )
+                ) {
+    constructor() : this(
+        0,
+        0,
+        "",
+        "",
+        Date(),
+        Date()
+    )
+}
