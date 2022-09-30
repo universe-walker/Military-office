07bc7cb36a9a954aa805bdc91e89799ce9559ced
universe-walker <nikitamotlich@gmail.com>
2022-09-30 21:05:48 +0300

    Изменил класс, сделал его дата классом, добавил пустой конструктор

--- src/main/java/com/nm/Militaryoffice/model/Conscription.kt
+++ src/main/java/com/nm/Militaryoffice/model/Conscription.kt
@@ -1,12 +1,21 @@
 package com.nm.Militaryoffice.model
 
+import java.time.LocalDate
 import java.util.*
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
-class Conscription(@NotNull var conscription_id: Int,
+data class Conscription(@NotNull var conscription_id: Int,
                    @NotEmpty var conscription_text: String,
                    @NotNull var start_date: Date,
                    var end_date: Date?,
                    @NotNull var quantity_plan: Int
-                   )
+                   ) {
+    constructor() : this(
+        0,
+        "",
+        Date(),
+        Date(),
+        0
+    )
+}
