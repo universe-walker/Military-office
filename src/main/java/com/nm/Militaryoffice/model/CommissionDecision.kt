6e8b8ce0b185ced11ac9271305709bd78a1f6fbb
universe-walker <nikitamotlich@gmail.com>
2022-09-30 20:55:20 +0300

    Изменил класс CommissionDecision, сделал дата классом, добавил пустой конструктор

--- src/main/java/com/nm/Militaryoffice/model/CommissionDecision.kt
+++ src/main/java/com/nm/Militaryoffice/model/CommissionDecision.kt
@@ -4,12 +4,22 @@ import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
-class CommissionDecision(
-        @NotNull decision_id: Int,
-        @NotNull id: Long,
-        @NotEmpty @Size(max = 500) decision_text: String,
-        @NotEmpty @Size(max = 300) organisation_that_made_decision: String,
-        @NotEmpty @Size(max = 100) commissioner_fullname: String,
-        @NotNull is_enlistment: Boolean,
-        is_conscript_signed: Boolean?
-)
+data class CommissionDecision(
+        @NotNull var  decision_id: Int,
+        @NotNull var id: Long,
+        @NotEmpty @Size(max = 500) var decision_text: String,
+        @NotEmpty @Size(max = 300) var organisation_that_made_decision: String,
+        @NotEmpty @Size(max = 100) var commissioner_fullname: String,
+        @NotNull var is_enlistment: Boolean,
+        var is_conscript_signed: Boolean?
+) {
+        constructor() : this(
+                0,
+                0,
+                "",
+                "",
+                "",
+                false,
+                null
+        )
+}
