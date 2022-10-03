698cfcbc00cdbd0c7900dfbcd4920364e72948d8
universe-walker <nikitamotlich@gmail.com>
2022-10-03 13:14:55 +0300

    Добавил класс DataSourceConfig

--- src/main/java/com/nm/Militaryoffice/config/DataSourceConfig.java
+++ src/main/java/com/nm/Militaryoffice/config/DataSourceConfig.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
